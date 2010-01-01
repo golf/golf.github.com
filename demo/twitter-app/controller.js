6e8f4c4b760b074c6ef1793dd356544ec16d5698
Micha Niskin <asdf@ubergibson.com>
2009-12-31 19:41:40 -0500

    sadf

--- demo/twitter-app/controller.js
+++ demo/twitter-app/controller.js
@@ -1,1 +1,16 @@
 $.golf.defaultRoute = "/search/golf/";
+
+$.golf.controller = [
+  { route: "^/search/(([^/]+)/)?$",
+    action: (function() {
+      var tweet;
+      return function(container, params) {
+        tweet = !!tweet ? tweet : new Component.Twitter();
+        if (!params[2])
+          return $.golf.location(params[0]+"golf/");
+        container.empty().append(tweet);
+        tweet.search(decodeURIComponent(params[2]));
+      };
+    })()
+  }
+];
