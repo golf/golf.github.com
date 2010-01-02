1c757ccbfd24636e0cd510858186b754466fbbed
Micha Niskin <asdf@ubergibson.com>
2010-01-02 01:53:27 -0500

    added fancytable demo

---
+++ demo/fancy-sortable-table/controller.js
@@ -0,0 +1,31 @@
+
+var data = [
+  [ "22", 
+    "first post", 
+    "<p>This is the first post.</p>"
+  ],
+  [ "23", 
+    "second post", 
+    "<p>This is the second post.</p>"
+  ],
+  [ "24", 
+    "fourth post", 
+    "<p>This is the fourth post.</p>"
+  ],
+  [ "25", 
+    "fifth post", 
+    "<p>This is the fifth post.</p>"
+  ]
+];
+
+$.golf.defaultRoute = "/test/";
+
+$.golf.controller = [
+
+  { route: ".*",
+    action: function(b,match) {
+      b.empty().append(new Component.TableExample(data));
+    }
+  }
+
+];
